67ae712b08889339a8902c4ea47f35e384d39692
pruth_17 <ppruthvi2017@gmail.com>
2020-06-29 00:09:17 -0700

    random dice completed

---
+++ Dicee Challenge/index.js
@@ -0,0 +1,23 @@
+var random1 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage1 = "images/dice" + random1 + ".png";
+
+var random2 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage2 =  "images/dice" + random2 + ".png";
+
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomDiceImage1);
+
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", randomDiceImage2);
+
+var heading = document.querySelector("h1");
+if(random1 > random2){
+  heading.innerHTML = "Player1 won!"
+}
+else if(random2 > random1){
+  heading.innerHTML = "Player2 won!"
+}
+
+else {
+  heading.innerHTML = "Draw!"
+}
