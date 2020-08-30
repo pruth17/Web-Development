a945c4177d93c0d532f1deb9ec83ce97f9a36046
pruth_17 <ppruthvi2017@gmail.com>
2020-08-29 21:58:57 -0700

    add new files

--- ToDoList/app.js
+++ ToDoList/app.js
@@ -4,9 +4,12 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get("/", function(req, res){
   res.send("Hello");
 });
+
 
 app.listen(3000, function(){
   console.log("Server started at port 3000");
