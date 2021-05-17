5997467bf998fac8b701360014a40103e8a02d35
pruth_17 <ppruthvi2017@gmail.com>
2021-05-17 14:31:23 -0700

    add changes

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -7,6 +7,9 @@ app.use(bodyParser.urlencoded({
   extended:true
 }));
 
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + '/public'));
+
 app.get("/", function(req, res) {
 
   res.sendFile(__dirname + "/index.html")
@@ -23,6 +26,17 @@ app.post("/", function(req, res){
     response.on("data", function(data){
 
       const weatherData = JSON.parse(data);
+
+      if(weatherData == null) {
+        res.write("<h1> Incorrect City </h1>");
+        res.send();
+      }
+      else {
+        if(weatherData.main == null){
+          res.write("<h1> Incorrect City </h1>");
+          res.send();
+        }
+        else {
       const temp = weatherData.main.temp;
       const description = weatherData.weather[0].description;
       const icon = weatherData.weather[0].icon;
@@ -31,6 +45,8 @@ app.post("/", function(req, res){
       res.write("<p>The weather is currently " + description + "</p>");
       res.write("<img src=" +  imgaeurl + ">");
       res.send();
+    }
+    }
     });
 
   });
